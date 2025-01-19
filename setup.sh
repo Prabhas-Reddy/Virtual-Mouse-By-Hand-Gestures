8fb169493f851fdcc512273c0f1103ebf564d978
Prabhas-Reddy <iamprabhas21@gmail.com>
2025-01-19 10:42:46 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Uninstall existing OpenCV version
-pip uninstall -y opencv-python-headless
-
-# Install the specific version of OpenCV
+pip uninstall -y opencv-python
 pip install opencv-python-headless==4.8.0.76
-
-# Install other dependencies listed in requirements.txt
-pip install -r requirements.txt
